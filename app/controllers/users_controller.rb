dd6b0510fe570137008a784cc8d192272297f59d
島崎塁 <mitsuba19930328@gmail.com>
2020-09-06 19:09:29 +0900

    finish profile wireframe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,9 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  # def destroy
+  #   current_user = nil
+  #   # 削除後、homeページに戻す
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root "top#index"
-  resources :users, only: %i(index show)
+  resources :users, only: %i(index show destroy)
 end
